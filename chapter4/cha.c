5717ecffb0964fc974c0face497fe40527efe156
marciojales <marcio.dojcosta@gmail.com>
2017-09-02 15:50:31 -0300

    Final version

--- chapter4/cha.c
+++ chapter4/cha.c
@@ -14,8 +14,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define BUF_SIZE 128
+#define BUF_SIZE 512
+#define MAX_NUMBER_FILES 12
 
 void parse_args(int argc, char** argv)
 {
@@ -33,15 +37,40 @@ void parse_args(int argc, char** argv)
 int main(int argc, char** argv)
 {
   char buffer[BUF_SIZE];
-  int bytes_read = -1;
+  int bytes_read = -1, file_count = 0;
+  int count, fd[MAX_NUMBER_FILES];
 
   parse_args(argc, argv);
 
-  /* That is: no files provided. optind = 1 means that no options were found */
-  if(argc < 3 && optind != 1) {
-    while(bytes_read) {
-      bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
-      write(STDOUT_FILENO, buffer, bytes_read);
+  if((argc > 2 || (argc == 2 && optind == 1))) {
+    if(argc > MAX_NUMBER_FILES + optind) {
+      printf("Too many files. Maximum of %d files.\n", MAX_NUMBER_FILES);
+      exit(EXIT_FAILURE);
     }
+
+    for(count = optind; count < argc ; count++) {
+      if(optind == 1)
+        fd[file_count++] = open(argv[count], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+      if(optind == 2)
+        fd[file_count++] = open(argv[count], O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+    }
+  }
+
+  /* That is: no files provided. optind = 1 means that no options were found */
+  while(bytes_read) {
+    bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
+
+    if((argc > 2 || (argc == 2 && optind == 1))) {
+      file_count = 0;
+      for(count = optind; count < argc ; count++)
+        write(fd[file_count++], buffer, bytes_read);
+    }
+    write(STDOUT_FILENO, buffer, bytes_read);
+  }
+
+  if((argc > 2 || (argc == 2 && optind == 1))) {
+    file_count = 0;
+    for(count = optind; count < argc ; count++)
+      close(fd[file_count++]);
   }
 }
