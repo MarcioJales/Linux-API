395b3142bb0da7df33372dc4db0f54a9d591013a
marciojales <marcio.dojcosta@gmail.com>
2017-09-02 02:15:42 -0300

    !

--- chapter4/mytee.c
+++ chapter4/mytee.c
@@ -17,9 +17,17 @@
 
 #define BUF_SIZE 128
 
-int parse_args(int argc, char** argv)
+void parse_args(int argc, char** argv)
 {
+  /* After getopt scans argv, "-a" becomes argv[1] */
+  int opt = getopt(argc, argv, "a");
 
+  while(opt != -1) {
+    if (opt == '?')
+      exit(EXIT_FAILURE);
+
+    opt = getopt(argc, argv, "a");
+  }
 };
 
 int main(int argc, char** argv)
@@ -27,8 +35,10 @@ int main(int argc, char** argv)
   char buffer[BUF_SIZE];
   int bytes_read = -1;
 
-  /* That is: no files provided */
-  if(argc < 3) {
+  parse_args(argc, argv);
+
+  /* That is: no files provided. optind = 1 means that no options were found */
+  if(argc < 3 && optind != 1) {
     while(bytes_read) {
       bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
       write(STDOUT_FILENO, buffer, bytes_read);
