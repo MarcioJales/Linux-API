6590b01e75931693ba90c0af1728bac7796020f0
Márcio Jales <marcio.dojcosta@gmail.com>
2018-03-28 18:14:09 -0300

    Missing the watch creation to new directories

--- inotify/log_dir.c
+++ inotify/log_dir.c
@@ -20,29 +20,58 @@
  #define _XOPEN_SOURCE 500
  #include <ftw.h>
 
- /* This value is defined in /proc/sys/fs/inotify/max_user_watches
-  * and is hardcoded here
-  */
- #define MAX_USER_WATCHES 8192
  #define BUF_LEN (12 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
- int instance_fd, watch_fd[MAX_USER_WATCHES];
- int wfd_count = 0;
+ int instance_fd, watch_fd;
 
  int addWatch(const char *pathname, const struct stat *file_info, int type, struct FTW *sftw)
  {
    if(type == FTW_D) {
-     watch_fd[wfd_count] = inotify_add_watch(instance_fd, pathname, IN_ALL_EVENTS);
-     printf("Watch [wfd = %d] added to %s\n", watch_fd[wfd_count], pathname);
-     wfd_count++;
+     watch_fd = inotify_add_watch(instance_fd, pathname, IN_CREATE | IN_DELETE | IN_MOVE);
+     printf("Watch [wfd = %d] added to %s\n", watch_fd, pathname);
    }
 
    return 0;
  }
 
+ void displayInotifyEvent(struct inotify_event *ievent)
+ {
+     printf("    wd = %2d; ", ievent->wd);
+     if (ievent->cookie > 0)
+         printf("cookie = %4d; ", ievent->cookie);
+
+     printf("mask = ");
+     if (ievent->mask & IN_ACCESS)        printf("IN_ACCESS ");
+     if (ievent->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+     if (ievent->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+     if (ievent->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+     if (ievent->mask & IN_DELETE)        printf("IN_DELETE ");
+     if (ievent->mask & IN_IGNORED)       printf("IN_IGNORED ");
+     if (ievent->mask & IN_ISDIR)         printf("IN_ISDIR ");
+     if (ievent->mask & IN_MODIFY)        printf("IN_MODIFY ");
+     if (ievent->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+     if (ievent->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+     if (ievent->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+     if (ievent->mask & IN_OPEN)          printf("IN_OPEN ");
+     if (ievent->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+     if (ievent->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+     if (ievent->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+     if (ievent->mask & IN_CREATE) {
+       printf("IN_CREATE ");
+       /* Need to add watch if a directory */
+     }
+     printf("\n");
+
+     if (ievent->len > 0)
+         printf("        name = %s\n", ievent->name);
+ }
+
  int main(int argc, char **argv)
  {
    char buffer[BUF_LEN];
+   ssize_t bytes_read;
+   char *event_pointer;
+   struct inotify_event *event;
 
    if(argc < 2) {
      fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
@@ -58,6 +87,23 @@
    if(nftw(argv[1], addWatch, 10, 0) == -1) {
      fprintf(stderr, "Error (nftw): %s\n", strerror(errno));
      exit(EXIT_FAILURE);
+   }
+
+   while(1) {
+     bytes_read = read(instance_fd, buffer, BUF_LEN);
+     if(bytes_read == 0)
+       printf("Nothing to read\n");
+     if(bytes_read == -1) {
+       fprintf(stderr, "Error (read): %s\n", strerror(errno));
+       exit(EXIT_FAILURE);
+     }
+     printf("Read %ld bytes from inotify fd\n", (long) bytes_read);
+
+     for(event_pointer = buffer; event_pointer < buffer + bytes_read; ) {
+       event = (struct inotify_event *) event_pointer;
+       displayInotifyEvent(event);
+       event_pointer += sizeof(struct inotify_event) + event->len;
+     }
    }
 
    return 0;
