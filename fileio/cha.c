a710d3404c3fceca6ca565cbee388f4423497b78
Márcio Jales <marcio.dojcosta@gmail.com>
2018-07-09 21:14:31 -0300

    Added have_files flag

--- fileio/cha.c
+++ fileio/cha.c
@@ -37,18 +37,21 @@ void parse_args(int argc, char** argv)
 int main(int argc, char** argv)
 {
   char buffer[BUF_SIZE];
-  int bytes_read = -1, file_count = 0;
+  int bytes_read = -1, file_count = 0, have_files = 0;
   int count, fd[MAX_NUMBER_FILES];
 
   parse_args(argc, argv);
 
-  if((argc > 2 || (argc == 2 && optind == 1))) {
+  if((argc > 2 || (argc == 2 && optind == 1)))
+    have_files = 1;
+
+  if(have_files) {
     if(argc > MAX_NUMBER_FILES + optind) {
       printf("Too many files. Maximum of %d files.\n", MAX_NUMBER_FILES);
       exit(EXIT_FAILURE);
     }
 
-    for(count = optind; count < argc ; count++) {
+    for(count = optind; count < argc; count++) {
       if(optind == 1)
         fd[file_count++] = open(argv[count], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
       if(optind == 2)
@@ -60,7 +63,7 @@ int main(int argc, char** argv)
   while(bytes_read) {
     bytes_read = read(STDIN_FILENO, buffer, BUF_SIZE);
 
-    if((argc > 2 || (argc == 2 && optind == 1))) {
+    if(have_files) {
       file_count = 0;
       for(count = optind; count < argc ; count++)
         write(fd[file_count++], buffer, bytes_read);
@@ -68,7 +71,7 @@ int main(int argc, char** argv)
     write(STDOUT_FILENO, buffer, bytes_read);
   }
 
-  if((argc > 2 || (argc == 2 && optind == 1))) {
+  if(have_files) {
     file_count = 0;
     for(count = optind; count < argc ; count++)
       close(fd[file_count++]);
