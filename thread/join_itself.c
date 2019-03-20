141df512d1727bc155a057b7dee07e00742b4718
Márcio Jales <marcio.dojcosta@gmail.com>
2019-03-19 22:07:43 -0300

    Calling pthread_join in itself

--- thread/join_itself.c
+++ thread/join_itself.c
@@ -7,3 +7,18 @@
  * Write a program to see what actually happens on Linux. If we have a variable, tid, containing a thread ID,
  * how can a thread prevent itself from making a call, pthread_join(tid, NULL), that is equivalent to the above statement?
  */
+
+#include <pthread.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  int ret;
+
+  ret = pthread_join(pthread_self(), NULL);
+  if(ret == EDEADLK)
+    printf("return %d = DEADLOCK\n", ret);
+
+  return 0;
+}
