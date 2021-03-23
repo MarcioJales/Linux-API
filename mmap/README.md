14b7febe0089178f1ed1236e1c9364d20e88571a
Márcio Jales <marcio.dojcosta@gmail.com>
2021-03-22 22:02:21 -0300

    [mmap-ex1] Check if there are two arguments (filenames expecteed)

--- mmap/README.md
+++ mmap/README.md
@@ -3,3 +3,5 @@
 | cpmap.c  | Exercise 1 from chapter 49 |
 
 ## Ch 49, Ex 1.
+
+According to the table 49.1 on the chapter, what we want (memory-mapped I/O) is achieved by a *shared file mapping*.
--- mmap/cpmap.c
+++ mmap/cpmap.c
@@ -2,3 +2,16 @@
  *
  * Write a program, analogous to cp(1), that uses mmap() and memcpy() calls (instead of read() or write()) to copy a source file to a destination file. (Use fstat() to obtain the size of the input file, which can then be used to size the required memory mappings, and use ftruncate() to set the size of the output file.)
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+  if(argc != 3) {
+    fprintf(stderr, "Give 2 filenames\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return 0;
+}
