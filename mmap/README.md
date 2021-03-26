82679e2e2b1d3fe425cdcfb2a7b4402406e5a1d6
Márcio Jales <marcio.dojcosta@gmail.com>
2021-03-25 23:22:23 -0300

    [mmap-ex3] Test the SIGSEGV behavior from figure 49-3

--- mmap/README.md
+++ mmap/README.md
@@ -9,3 +9,9 @@ According to the table 49.1 on the chapter, what we want (memory-mapped I/O) is 
 More information about the suggested functions `fstat()` and `ftruncate()` can be found on the book at chapters 15 and 5, respectively.
 
 ## Ch 49, Ex 3.
+
+Files were created using `dd`:
+
+```
+$ dd if=/dev/zero of=case1.txt bs=1 count=6000
+```
--- mmap/mmap_sig.c
+++ mmap/mmap_sig.c
@@ -2,3 +2,29 @@
  *
  * Write programs to verify that the SIGBUS and SIGSEGV signals are delivered in the circumstances described in Section 49.4.3.
  */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main() {
+  char *addr;
+  int fd;
+
+  /* Example from figure 49-3 */
+  /* Page size multiple is allocated (8192). SIGSEGV after */
+  fd = open("case1.txt", O_RDWR);
+  addr = mmap(NULL, 6000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  
+  printf("Write 'a' to (addr + 2000): %p\n", addr + 2000);
+  *(addr + 2000) = 'a';
+  printf("(addr + 2000) = %c\n", *(addr + 2000));
+
+  printf("Write 'b' to (addr + 7000): %p\n", addr + 7000);
+  *(addr + 7000) = 'b';
+  printf("(addr + 7000) = %c\n", *(addr + 7000));
+
+  printf("Write 'c' to (addr + 9200): %p\n", addr + 9200);
+  *(addr + 9200) = 'c';
+  printf("(addr + 9200) = %c\n", *(addr + 9200));
+}
