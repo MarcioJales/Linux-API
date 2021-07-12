a7530f32d43c0561b30bdb29658677152fd350b7
Márcio Jales <marcio.dojcosta@gmail.com>
2021-07-12 15:33:38 -0300

    [ch20-ex2] Install the disposition that counts number of signals received

--- signals/sig_ign.c
+++ signals/sig_ign.c
@@ -8,14 +8,26 @@
 #include "signal_functions.h"           /* Declaration of printSigset() */
 #include "tlpi_hdr.h"
 
+static int sigCnt = 0; 
+
 static void
 handler(int sig)
 {
+    sigCnt++;
 }
 
 int
 main(int argc, char *argv[])
 {
     const int sigNumber = 10; /* Hardcoded SIGUSR1 */
-    const int numSecs = 20;   
+    const int numSecs = 20;
+    struct sigaction sa;
+
+    printf("%s: PID is %ld\n", argv[0], (long) getpid());
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;          
+    sa.sa_handler = handler;
+
+    (void) sigaction(sigNumber, &sa, NULL);    
 }
