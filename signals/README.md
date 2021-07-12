1c5521235eadfda6d3b23fdadabb8198faa29c4e
Márcio Jales <marcio.dojcosta@gmail.com>
2021-07-12 14:25:06 -0300

    [ch20-ex2] Add files

--- signals/README.md
+++ signals/README.md
@@ -1,6 +1,7 @@
 | Name | Assignment |
 | ---- | ---------- |
 | sig_receiver.c  | Exercise 1 from chapter 20 |
+| sig_ign.c  | Exercise 2 from chapter 20 |
 
 ## Ch 20, Ex 1.
 
---
+++ signals/sig_ign.c
@@ -0,0 +1,4 @@
+/* Chapter 20, exercise 2
+ *
+ * Write a program that shows that when the disposition of a pending signal is changed to be SIG_IGN, the program never sees (catches) the signal.
+ */
