db86fb96f6e37e0bb38c1d1476c8c784e0039f5f
Márcio Jales <marcio.dojcosta@gmail.com>
2021-07-11 21:25:58 -0300

    [ch20-ex1] Add the source code file

---
+++ signals/sig_receiver.c
@@ -0,0 +1,5 @@
+/* Chapter 20, exercise 1
+ *
+ * As noted in Section 20.3, sigaction() is more portable than signal() for establishing a signal handler. 
+ * Replace the use of signal() by sigaction() in the program * in Listing 20-7 (sig_receiver.c).
+ */
