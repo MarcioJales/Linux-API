b9cc8dd9b3bc6eca09f7fb52851db271adf8d08f
Márcio Jales <marcio.dojcosta@gmail.com>
2021-07-14 18:56:01 -0300

    [ch20-ex3] SA_RESETHAND logic

--- signals/flags_effect.c
+++ signals/flags_effect.c
@@ -6,24 +6,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void
+handler()
+{
+    fprintf(stdout, "SIGINT brought us here\n");;
+}
 
 int main(int argc, char *argv[]) 
 {
-  if(argc != 2) {
-    fprintf(stderr, "Provide 1 argument, either SA_RESETHAND or SA_NODEFER\n");
-    exit(EXIT_FAILURE);
-  }
+    struct sigaction sa;
 
-  if(!strcmp(argv[1], "SA_RESETHAND")) {
-    fprintf(stdout, "SA_RESETHAND\n");
-  }
-  else if(!strcmp(argv[1], "SA_NODEFER")) {
-    fprintf(stdout, "SA_NODEFER\n");
-  }
-  else {
-    fprintf(stderr, "Please, provide either SA_RESETHAND or SA_NODEFER\n");
-    exit(EXIT_FAILURE);
-  }
+    sigemptyset(&sa.sa_mask);          
+    sa.sa_handler = handler;
 
-  exit(EXIT_SUCCESS);
+    if(argc != 2) {
+        fprintf(stderr, "Provide 1 argument, either SA_RESETHAND or SA_NODEFER\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(!strcmp(argv[1], "SA_RESETHAND")) {
+        /* To test the flag, we first install a handler that simply prints a message 
+        ** We use SIGINT in this example
+        */
+        sa.sa_flags = SA_RESETHAND;
+
+        (void) sigaction(SIGINT, &sa, NULL); 
+
+        printf("%ld: Paused. Use 'kill' to send SIGINT\n", (long) getpid());
+        pause();
+
+        /* Now, we expect that no message is printed */
+        printf("%ld: Paused. If you send SIGINT now, I'm going to exit!\n", (long) getpid());
+        pause();
+    }
+    else if(!strcmp(argv[1], "SA_NODEFER")) {
+        fprintf(stdout, "SA_NODEFER\n");
+    }
+    else {
+        fprintf(stderr, "Please, provide either SA_RESETHAND or SA_NODEFER\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
 }
