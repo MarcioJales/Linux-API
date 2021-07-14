e4902ca99d47af3116f917c0c7c84068237b7982
Márcio Jales <marcio.dojcosta@gmail.com>
2021-07-14 16:57:05 -0300

    [ch20-ex3] if/else to select the argument provided

--- signals/flags_effect.c
+++ signals/flags_effect.c
@@ -5,11 +5,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) 
 {
   if(argc != 2) {
     fprintf(stderr, "Provide 1 argument, either SA_RESETHAND or SA_NODEFER\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(!strcmp(argv[1], "SA_RESETHAND")) {
+    fprintf(stdout, "SA_RESETHAND\n");
+  }
+  else if(!strcmp(argv[1], "SA_NODEFER")) {
+    fprintf(stdout, "SA_NODEFER\n");
+  }
+  else {
+    fprintf(stderr, "Please, provide either SA_RESETHAND or SA_NODEFER\n");
     exit(EXIT_FAILURE);
   }
 
